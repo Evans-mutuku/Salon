88e7cccf84b20a7b9efc3497e0ec6c25dd6964aa
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-05 22:35:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcXf30oyS1KWVSSHryLy4mInzLVQWW3D0",
+  authDomain: "salon-8a54d.firebaseapp.com",
+  projectId: "salon-8a54d",
+  storageBucket: "salon-8a54d.appspot.com",
+  messagingSenderId: "736582705075",
+  appId: "1:736582705075:web:12a6619250c8c6cb6875c6",
+  measurementId: "G-RE6FDCQ7VW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
